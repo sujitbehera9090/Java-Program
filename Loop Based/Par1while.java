6535f086638e042553bffed6c798893ea7c1c1a1
sujitbehera9090 <sujitbehera9979@gmail.com>
2022-01-02 23:50:44 +0530

    Print 1 to 10 using while loop

---
+++ Loop Based/Par1while.java
@@ -0,0 +1,19 @@
+
+
+import java.io.*;
+
+class Par1while{
+
+    public static void main( String arg[] ){
+
+        int i = 1;
+
+        while( i <= 10){
+
+            System.out.println( i + "\n");
+
+            i++;
+            
+        }
+    }
+}
