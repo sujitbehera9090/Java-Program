df17e3df02f74c58fe2257320f9380419e18f064
sujitbehera9090 <sujitbehera9979@gmail.com>
2022-01-02 23:17:59 +0530

    Fibonansi Series

---
+++ Basic program/Fibonasci_series.java
@@ -0,0 +1,24 @@
+
+
+import java.util.*;
+import java.util.*;
+
+class Fibonasci_series{
+
+    public static void main( String arg[] ){
+
+        int range , n1 = 0 , n2 = 1 , fibo;
+
+        for( int i = 1; i <= range; i++ ){
+
+            fibo = n1 + n2;
+
+            n1 = n2;
+
+            n2 = fibo;
+
+        }
+
+        
+    }
+}
