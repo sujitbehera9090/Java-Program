72c92ca43beec78e80c691d98a2150363caaeef5
sujitbehera9090 <sujitbehera9979@gmail.com>
2022-01-03 12:08:25 +0530

    Print 100 to 50 in reverse order using do while loop

---
+++ Loop Based/Par2do.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+
+class Par2do{
+
+    public static void main( String arg[] ){
+
+        int i = 100;
+
+        do{
+
+            System.out.println( i + "\n");
+
+            i--;
+
+        }
+
+        while( i >= 50 );
+        
+    }
+}
