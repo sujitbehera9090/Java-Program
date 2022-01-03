987d2e5bcd371779d1d665a71904dbde0909836e
sujitbehera9090 <sujitbehera9979@gmail.com>
2022-01-03 12:24:44 +0530

    Print 100 to 50 in reverse using for loop

---
+++ Loop Based/Par2for.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+
+class Par2for{
+
+    public static void main( String arg[] ){
+
+        int i = 100;
+
+        for( ; i >= 50 ; i-- ){
+
+            System.out.println( i + "\n" );
+
+        }
+
+    }
+
+}
