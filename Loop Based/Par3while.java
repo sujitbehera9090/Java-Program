7b1379554daa7748b56d36646cb8ce7147d85451
sujitbehera9090 <sujitbehera9979@gmail.com>
2022-01-04 22:57:48 +0530

    Print all even numbers between 500 to 700 using  while loop

---
+++ Loop Based/Par3while.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+
+class Par3while{
+
+    public static void main( String arg[] ){
+
+        int i = 500;
+
+        while( i <= 700){
+
+            if( i % 2 == 0 ){
+
+                System.out.println( i + "\n" );
+
+            }
+
+            i++;
+
+        }
+
+    }
+
+}
